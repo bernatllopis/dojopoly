b90b27e0051749561465bd3f501b3ca3185f9918
Bernat <40668639+bernatllopis@users.noreply.github.com>
2026-04-26 17:01:57 +0200

    game.js

--- game.js
+++ game.js
@@ -6,19 +6,57 @@ tablero.src = "tablero.png";
 
 // Coordenadas aproximadas de cada casilla (x, y)
 const casillas = [
-    {x: 700, y: 700}, // Casilla 1
-    {x: 600, y: 700}, // Casilla 2
-    {x: 500, y: 700}, // Casilla 3
-    {x: 400, y: 700}, // Casilla 4
-    {x: 300, y: 700}, // Casilla 5
-    {x: 200, y: 700}, // Casilla 6
-    {x: 100, y: 700}, // Casilla 7
-    {x: 100, y: 600}, // Casilla 8
-    {x: 100, y: 500}, // Casilla 9
-    {x: 100, y: 400}, // Casilla 10
-    {x: 100, y: 300}, // Casilla 11
-    {x: 100, y: 200}  // Casilla 12
+    // Abajo (de derecha a izquierda)
+    {x: 720, y: 720}, // 1
+    {x: 630, y: 720}, // 2
+    {x: 560, y: 720}, // 3
+    {x: 490, y: 720}, // 4
+    {x: 420, y: 720}, // 5
+    {x: 350, y: 720}, // 6
+    {x: 280, y: 720}, // 7
+    {x: 210, y: 720}, // 8
+    {x: 140, y: 720}, // 9
+    {x: 70,  y: 720}, // 10
+
+    // Izquierda (de abajo a arriba)
+    {x: 70,  y: 630}, // 11
+    {x: 70,  y: 560}, // 12
+    {x: 70,  y: 490}, // 13
+    {x: 70,  y: 420}, // 14
+    {x: 70,  y: 350}, // 15
+    {x: 70,  y: 280}, // 16
+    {x: 70,  y: 210}, // 17
+    {x: 70,  y: 140}, // 18
+    {x: 70,  y: 70},  // 19
+
+    // Arriba (de izquierda a derecha)
+    {x: 140, y: 70},  // 20
+    {x: 210, y: 70},  // 21
+    {x: 280, y: 70},  // 22
+    {x: 350, y: 70},  // 23
+    {x: 420, y: 70},  // 24
+    {x: 490, y: 70},  // 25
+    {x: 560, y: 70},  // 26
+    {x: 630, y: 70},  // 27
+    {x: 720, y: 70},  // 28
+
+    // Derecha (de arriba a abajo)
+    {x: 720, y: 140}, // 29
+    {x: 720, y: 210}, // 30
+    {x: 720, y: 280}, // 31
+    {x: 720, y: 350}, // 32
+    {x: 720, y: 420}, // 33
+    {x: 720, y: 490}, // 34
+    {x: 720, y: 560}, // 35
+    {x: 720, y: 630}, // 36
+
+    // Última casilla antes de volver al inicio
+    {x: 720, y: 700}, // 37
+    {x: 720, y: 720}, // 38 (repetida para cerrar el ciclo)
+    {x: 720, y: 720}, // 39
+    {x: 720, y: 720}  // 40
 ];
+
 
 function dibujarFicha() {
     const pos = casillas[posicion - 1];
